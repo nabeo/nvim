d3451048e6e869ba92d00928f44c42cb0a836f52
nabeo <watanabe.michikazu@gmail.com>
2024-10-20 22:08:13 +0900

    use neogit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,4 +3,14 @@ return {
       'lewis6991/gitsigns.nvim',
       event = 'BufRead',
    },
+   {
+      "NeogitOrg/neogit",
+      dependencies = {
+         "nvim-lua/plenary.nvim",         -- required
+         "sindrets/diffview.nvim",        -- optional - Diff integration
+
+         "nvim-telescope/telescope.nvim", -- optional
+      },
+      config = true
+   },
 }

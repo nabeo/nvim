9b8145437ed7f1808e18a6fafbe8c1317abe365b
Michikazu Watanabe <michikazu.watanabe@henry.jp>
2024-10-22 14:32:16 +0900

    use tokyonight as colorscheme

---
+++ lua/plugins/colorschema.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.cmd [[colorscheme tokyonight]]
+    end,
+    opts = {
+      style = 'day'
+    },
+  }
+}

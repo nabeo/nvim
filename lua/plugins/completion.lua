7cd51274fd1828d033bbb6eb83a0fac6f7607e42
nabeo <watanabe.michikazu@gmail.com>
2024-10-26 19:24:51 +0900

    add dependencies to nvim-cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -5,6 +5,8 @@ return {
     dependencies = {
       { "hrsh7th/cmp-nvim-lsp" },
       { "hrsh7th/vim-vsnip" },
+      { "hrsh7th/cmp-path" },
+      { "hrsh7th/cmp-buffer" },
     },
     config = function()
       local cmp = require("cmp")

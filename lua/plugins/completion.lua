d93d9f1fcc9b1b8a9bf3a5f92319908982242310
nabeo <watanabe.michikazu@gmail.com>
2024-10-26 15:17:41 +0900

    add nvim-cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,5 +1,36 @@
 -- 補完関係
 return {
-  -- TODO: hrsh7th/nvim-cmp
-  -- TODO: hrsh7th/cmp-nvim-lsp
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/vim-vsnip" },
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      require("cmp").setup {
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-l>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm { select = true },
+        }),
+        experimental = {
+          ghost_text = true,
+        }
+      }
+    end
+  },
 }

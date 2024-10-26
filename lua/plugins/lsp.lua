da6ebc0e7bbb5fd954cd1f502139b5004b8611b0
Michikazu Watanabe <michikazu.watanabe@henry.jp>
2024-10-26 09:52:41 +0900

    setup mason-lspconfig for typescript and terraform

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,36 @@
 return {
-  -- TODO: lua-lspconfig: LSP 設定
-  -- TODO: mason: LSP サーバ管理
-  -- TODO: mason-lspconfig
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    { "williamboman/mason.nvim" },
+    { "neovim/nvim-lspconfig" },
+  },
+  config = function()
+    require("mason").setup()
+
+    -- https://github.com/williamboman/mason.nvim/issues/1309#issuecomment-1555018732
+    local registry = require "mason-registry"
+    local packages = {
+      "typescript-language-server",
+    }
+    registry.refresh(function()
+      for _, pkg_name in ipairs(packages) do
+        local pkg = registry.get_package(pkg_name)
+        if not pkg:is_installed() then
+          pkg:install()
+        end
+      end
+    end)
+
+    require("mason-lspconfig").setup()
+
+    require("lspconfig").ts_ls.setup {
+      init_options = {
+      },
+      filetypes = {
+        "javascript",
+        "typescript",
+      },
+    }
+    require("lspconfig").terraformls.setup {}
+  end
 }

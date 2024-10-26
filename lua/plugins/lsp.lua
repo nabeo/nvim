481930b09f780b0c897a874781cbfcf478e0872b
nabeo <watanabe.michikazu@gmail.com>
2024-10-26 18:28:18 +0900

    add config for yamlls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -103,9 +103,21 @@ return {
     require("lspconfig").yamlls.setup {
       settings = {
         yaml = {
+          schemaStore = {
+            enable = true,
+            url = "https://www.schemastore.org/api/json/catalog.json",
+          },
           schemas = {
             ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
           },
+          format = {
+            enable = true,
+            singleQuote = true,
+            backetSpacing = true,
+          },
+          validate = true,
+          hover = true,
+          completion = true,
         },
       }
     }

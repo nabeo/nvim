2b73aa3dcd34e9cd70873a3b001ac050775a9e55
nabeo <watanabe.michikazu@gmail.com>
2024-11-02 18:44:43 +0900

    emacs keybind

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("config.base")
 require("config.keymaps")
+require("config.emacs-keymaps")
 require("config.lazy")
---
+++ lua/config/emacs-keymaps.lua
@@ -0,0 +1,7 @@
+-- Telescope
+vim.keymap.set('n', '<C-x><C-f>', ':Telescope find_files')
+vim.keymap.set('n', '<C-x>b', ':Telescope buffers')
+vim.keymap.set('n', '<C-x><C-b>', ':Telescope buffers')
+
+-- Neogit
+vim.keymap.set('n', '<C-x><C-g>', ':Neogit')

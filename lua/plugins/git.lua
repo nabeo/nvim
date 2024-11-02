0c0519701f26693eaa08314824ce043a0645129c
nabeo <watanabe.michikazu@gmail.com>
2024-11-02 15:48:55 +0900

    add f-person/git-blame.nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,16 +1,26 @@
 return {
-   {
-      'lewis6991/gitsigns.nvim',
-      event = 'BufRead',
-   },
-   {
-      "NeogitOrg/neogit",
-      dependencies = {
-         "nvim-lua/plenary.nvim",         -- required
-         "sindrets/diffview.nvim",        -- optional - Diff integration
+  {
+     'lewis6991/gitsigns.nvim',
+     event = 'BufRead',
+  },
+  {
+    'f-person/git-blame.nvim',
+    event = 'VeryLazy',
+    opts = {
+      enabled = true,
+      message_template = ' <summary> . <date> . <author> . <<sha>>',
+      date_format = '%Y/%m/%d %H:%M:%S',
+      virtual_text_colum = 1,
+    }
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+       "nvim-lua/plenary.nvim",         -- required
+       "sindrets/diffview.nvim",        -- optional - Diff integration
 
-         "nvim-telescope/telescope.nvim", -- optional
-      },
-      config = true
-   },
+       "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true
+  },
 }

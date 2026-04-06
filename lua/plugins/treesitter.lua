0135edd6a0d2b7e10e5ea24cc8034883807fd7bd
Michikazu Watanabe <michikazu.watanabe@henry.jp>
2026-04-06 21:10:34 +0900

    neovim 0.12 にあげたので nvim-treesitter を master ブランチ版から main ブランチ版に変更した

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,47 +1,63 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    main = 'nvim-treesitter.configs',
+    branch = 'main',
+    main = 'nvim-treesitter',
     envent = { 'BufReadPost', 'BufNewfile' },
     build = function()
       require('nvim-treesitter.install').update({ with_sync = true })
     end,
+    init = function ()
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function ()
+          --- Enable treesitter highlighting and disable regex syntax
+          pcall(vim.treesitter.start)
+          --- Enable treesitter-based indentionn
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+      local ensureInstalled = {
+        'bash',
+        'lua',
+        'diff',
+        'graphql',
+        'hcl',
+        'html',
+        'java',
+        'javascript',
+        'jsdoc',
+        'json',
+        'jsonc',
+        'jsonnet',
+        'kotlin',
+        'lua',
+        'luadoc',
+        'luap',
+        'markdown',
+        'markdown_inline',
+        'printf',
+        'python',
+        'query',
+        'regex',
+        'terraform',
+        'toml',
+        'tsx',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'xml',
+        'yaml',
+      }
+      local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+      local parsersToInstall = vim.iter(ensureInstalled)
+        :filter(function (parser)
+          return not vim.tbl_contains(alreadyInstalled, parser)
+        end)
+        :totable()
+      require('nvim-treesitter').install(parsersToInstall)
+    end,
     config = function()
-      require('nvim-treesitter.configs').setup({
-        highlight = { enable = true, },
-        indent = { enable = true, },
-        ensure_installed = {
-          'bash',
-          'lua',
-          'diff',
-          'graphql',
-          'hcl',
-          'html',
-          'java',
-          'javascript',
-          'jsdoc',
-          'json',
-          'jsonc',
-          'jsonnet',
-          'kotlin',
-          'lua',
-          'luadoc',
-          'luap',
-          'markdown',
-          'markdown_inline',
-          'printf',
-          'python',
-          'query',
-          'regex',
-          'terraform',
-          'toml',
-          'tsx',
-          'typescript',
-          'vim',
-          'vimdoc',
-          'xml',
-          'yaml',
-        },
+      require('nvim-treesitter').setup({
         incremental_selection = {
           enable = true,
           keymaps = {
